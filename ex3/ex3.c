3f2217cf03f915a0527562533d509972de048958
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-19 16:56:03 -0300

    Exercise 3

---
+++ ex3/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+  int age = 26;
+  int height = 170;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d cm tall.\n", height);
+
+
+  return 0;
+}
