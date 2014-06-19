73f07ec2c79dd1f1dc30d87a89928d199d74fed9
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-19 13:33:18 -0300

    Exercise 2

---
+++ ex2/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("This is me taking lessons from Zed ;)");
+  puts("He's a sexy opinionated bald dude!");
+  puts("This course should be fun");
+  puts("I need to get back to C basics.");
+
+  return 0;
+}
